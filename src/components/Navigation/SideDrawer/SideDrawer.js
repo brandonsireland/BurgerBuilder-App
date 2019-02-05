2768a3f1d8a4e536241b08e8e39e1d07b7bc4793
Brandon Ireland <brandonsireland@gmail.com>
2019-02-05 12:53:42 -0800

    Moved Layout to container folder

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,8 +4,8 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import BackDrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
-import aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
+
 
 const sideDrawer = ( props ) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
