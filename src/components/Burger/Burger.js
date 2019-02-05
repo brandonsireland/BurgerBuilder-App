e83ec94100261c2a5312aecc5c0ae9ba5db17b50
Brandon Ireland <brandonsireland@gmail.com>
2019-02-04 17:17:33 -0800

    added message if ingredients is zero

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,15 +5,24 @@ import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    // Takes ingredients and takes the key-value pairs into an Array.
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientKey => {
             return [...Array(props.ingredients[ingredientKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
                 });
-        });
-
-    return(
+        })
+        // flattens our array to check if the length = 0
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
+    console.log(transformedIngredients)
+    return (
         <div className={ classes.Burger }>
             <BurgerIngredient type="bread-top"/>
                 { transformedIngredients }
