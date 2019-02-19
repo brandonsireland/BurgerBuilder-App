be5afe46ce638ffb87986b6460fd605fe9341482
Brandon Ireland <brandonsireland@gmail.com>
2019-02-18 17:58:12 -0800

    removed old interceptors

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+ 
+
   render() {
     return (
       <div>
